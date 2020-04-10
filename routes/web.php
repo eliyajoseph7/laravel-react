95301c7749abbce0f50074a9aa253789419b12b3
eliyajoseph7 <exaveryeliya20@gmail.com>
2020-04-10 15:07:59 +0300

    route added

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\News;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,6 +11,12 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/new', function(){
+    $new = News::all();
+    return $new;
 });
+Route::view('/{path?}', 'welcome');
+
