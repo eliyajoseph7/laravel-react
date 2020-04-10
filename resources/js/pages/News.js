635b98caac3e1831d815f45110ecd6ea39f15556
eliyajoseph7 <exaveryeliya20@gmail.com>
2020-04-10 15:11:44 +0300

    layout changed

--- resources/js/pages/News.js
+++ resources/js/pages/News.js
@@ -1,11 +1,174 @@
 import React, { Component } from 'react'
+import wasa1 from '../img/wasa1.jpg'
+import wasa2 from '../img/wasa2.jpg'
+import wasa3 from '../img/wasa3.jpg'
+import axios from 'axios'
 
 class News extends Component {
+    constructor(){
+        super()
+        this.state={
+            news:[]
+        }
+    }
+
+    componentDidMount(){
+        axios.get('http://127.0.0.1:8000/new')
+            .then(response=>{
+                this.setState({news:response.data})
+            });
+    }
+
     render () {
         return (
-            <div>
-                This is News page...
-            </div>
+                <div className="content_container">
+                    
+                    <header>
+                        <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+                        <ol className="carousel-indicators">
+                            <li data-target="#carouselExampleIndicators" data-slide-to={0} className="active" />
+                            <li data-target="#carouselExampleIndicators" data-slide-to={1} />
+                            <li data-target="#carouselExampleIndicators" data-slide-to={2} />
+                        </ol>
+                        <div className="carousel-inner" role="listbox">
+                            {/* Slide One - Set the background image for this slide in the line below */}
+                            <div className="carousel-item active" style={{backgroundImage: 'url("http://placehold.it/1900x1080")'}}>
+                            <img src={wasa1} />
+
+                            <div className="carousel-caption d-none d-md-block">
+                                <h3>First Slide</h3>
+                                <p>This is a description for the first slide.</p>
+                            </div>
+                            </div>
+                            {/* Slide Two - Set the background image for this slide in the line below */}
+                            <div className="carousel-item" style={{backgroundImage: 'url("http://placehold.it/1900x1080")'}}>
+                            <img src={wasa2} />
+
+                            <div className="carousel-caption d-none d-md-block">
+                                <h3>Second Slide</h3>
+                                <p>This is a description for the second slide.</p>
+                            </div>
+                            </div>
+                            {/* Slide Three - Set the background image for this slide in the line below */}
+                            <div className="carousel-item" style={{backgroundImage: 'url("http://placehold.it/1900x1080")'}}>
+                            <img src={wasa3} />
+
+                            <div className="carousel-caption d-none d-md-block">
+                                <h3>Third Slide</h3>
+                                <p>This is a description for the third slide.</p>
+                            </div>
+                            </div>
+                        </div>
+                        <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                            <span className="carousel-control-prev-icon" aria-hidden="true" />
+                            <span className="sr-only">Previous</span>
+                        </a>
+                        <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                            <span className="carousel-control-next-icon" aria-hidden="true" />
+                            <span className="sr-only">Next</span>
+                        </a>
+                        </div>
+                    </header>
+                    {/* Page Content */}
+                    <div className="container">
+                        <h1 className="my-4">Welcome to Modern Business</h1>
+                        {/* Marketing Icons Section */}
+                        <div className="row">
+                        <div className="col-lg-4 mb-4">
+                            <div className="card h-100">
+                            <h4 className="card-header">Card Title</h4>
+                            <div className="card-body">
+                                <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+                            </div>
+                            <div className="card-footer">
+                                <a href="#" className="btn btn-primary">Learn More</a>
+                            </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 mb-4">
+                            <div className="card h-100">
+                            <h4 className="card-header">Card Title</h4>
+                            <div className="card-body">
+                                <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis ipsam eos, nam perspiciatis natus commodi similique totam consectetur praesentium molestiae atque exercitationem ut consequuntur, sed eveniet, magni nostrum sint fuga.</p>
+                            </div>
+                            <div className="card-footer">
+                                <a href="#" className="btn btn-primary">Learn More</a>
+                            </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 mb-4">
+                            <div className="card h-100">
+                            <h4 className="card-header">Card Title</h4>
+                            <div className="card-body">
+                                <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+                            </div>
+                            <div className="card-footer">
+                                <a href="#" className="btn btn-primary">Learn More</a>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                        {/* /.row */}
+                        {/* Portfolio Section */}
+                        <h2>Portfolio Heading</h2>
+                        <div className="row">
+                            {
+                                this.state.news.map(info=>{
+                                    return (
+                                        
+                                                <div className="col-lg-4 col-sm-6 portfolio-item">
+                                                    <div className="card h-100">
+                                                    <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt /></a>
+                                                        <div className="card-body">
+                                                            <h4 className="card-title">
+                                                            <a href="#">{info.title}</a>
+                                                            </h4>
+                                                            <p className="card-text">{info.updates}</p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                        
+                                    )
+                                })
+                                    
+                            }
+                        </div>
+                        
+                        {/* /.row */}
+                        {/* Features Section */}
+                        <div className="row">
+                        <div className="col-lg-6">
+                            <h2>Modern Business Features</h2>
+                            <p>The Modern Business template by Start Bootstrap includes:</p>
+                            <ul>
+                            <li>
+                                <strong>Bootstrap v4</strong>
+                            </li>
+                            <li>jQuery</li>
+                            <li>Font Awesome</li>
+                            <li>Working contact form with validation</li>
+                            <li>Unstyled page elements for easy customization</li>
+                            </ul>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, omnis doloremque non cum id reprehenderit, quisquam totam aspernatur tempora minima unde aliquid ea culpa sunt. Reiciendis quia dolorum ducimus unde.</p>
+                        </div>
+                        <div className="col-lg-6">
+                            <img className="img-fluid rounded" src="http://placehold.it/700x450" alt />
+                        </div>
+                        </div>
+                        {/* /.row */}
+                        <hr />
+                        {/* Call to Action Section */}
+                        <div className="row mb-4">
+                        <div className="col-md-8">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, expedita, saepe, vero rerum deleniti beatae veniam harum neque nemo praesentium cum alias asperiores commodi.</p>
+                        </div>
+                        <div className="col-md-4">
+                            <a className="btn btn-lg btn-secondary btn-block" href="#">Call to Action</a>
+                        </div>
+                        </div>
+                    </div>
+                    {/* /.container */}
+                </div>
         )
     }
 }
