9c81c159ec6c97c0a0d96e577a29132cfecf3b52
eliyajoseph7 <exaveryeliya20@gmail.com>
2020-04-11 18:54:02 +0300

    layout modified

--- resources/js/pages/News.js
+++ resources/js/pages/News.js
@@ -116,18 +116,64 @@ class News extends Component {
                                 this.state.news.map(info=>{
                                     return (
                                         
-                                                <div className="col-lg-4 col-sm-6 portfolio-item">
+                                                <div className=" col-lg-4 col-sm-6  portfolio-item">
                                                     <div className="card h-100">
-                                                    <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt /></a>
-                                                        <div className="card-body">
-                                                            <h4 className="card-title">
-                                                            <a href="#">{info.title}</a>
-                                                            </h4>
-                                                            <p className="card-text">{info.updates}</p>
+                                                    <a className="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+                                                        <div className="portfolio-hover">
+                                                            <div className="portfolio-hover-content">
+                                                                <i className="fas fa-plus fa-3x" />
+                                                            </div>
+                                                        </div>
+                                                        <img className="img-fluid card-img-top" src={wasa3} alt />
+                                                    </a>
+
+                                                        {/* <a href="#"><img className="card-img-top wave" src="http://placehold.it/700x400" alt /></a> */}
+                                                            <div className="card-body">
+                                                                <h4 className="card-title">
+                                                                <a href="#">{info.title}</a>
+                                                                </h4>
+                                                                <p className="card-text">{info.updates}</p>
+                                                            </div>
+                                                    </div>
+
+
+                                                     {/* Portfolio Modals */}
+                                                     {/* Modal 1 */}
+                                                    <div className="portfolio-modal modal fade" id="portfolioModal1" tabIndex={-1} role="dialog" aria-hidden="true">
+                                                        <div className="modal-dialog">
+                                                            <div className="modal-content">
+                                                                <div className="close-modal" data-dismiss="modal">
+                                                                    <div className="lr">
+                                                                    <div className="rl" />
+                                                                    </div>
+                                                                </div>
+                                                                <div className="container">
+                                                                    <div className="row">
+                                                                        <div className="col-lg-8 mx-auto">
+                                                                            <div className="modal-body">
+                                                                            {/* Project Details Go Here */}
+                                                                            <h2 className="text-uppercase">Project Name</h2>
+                                                                            <p className="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
+                                                                            <img className="img-fluid d-block mx-auto" src={wasa3} alt />
+                                                                            <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate,
+                                                                                maiores repudiandae, nostrum, reiciendis facere nemo!</p>
+                                                                            <ul className="list-inline">
+                                                                                <li>Date: January 2017</li>
+                                                                                <li>Client: Threads</li>
+                                                                                <li>Category: Illustration</li>
+                                                                            </ul>
+                                                                            <button className="btn btn-primary" data-dismiss="modal" type="button">
+                                                                                <i className="fas fa-times" />
+                                                                                Close Project</button>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
                                                         </div>
                                                     </div>
-                                                </div>
-                                        
+                                                </div>    
+                                  
                                     )
                                 })
                                     
