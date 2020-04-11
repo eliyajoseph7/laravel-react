0aa9cf0dddf99f0647ca8cf01fa257c9010e1a7b
eliyajoseph7 <exaveryeliya20@gmail.com>
2020-04-11 18:56:16 +0300

    links to some css files added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,24 +2,36 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <title>Laravel</title>
+
+        <title>MORUWASA</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <!-- Fontawesome 5 -->
+        <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+
 
         <link href="{{url('css/app.css')}}" rel="stylesheet">
-        <!-- <link href="{{url('css/mdb.css')}}" rel="stylesheet"> -->
+        <!-- Favicon-->
+        <link rel="shortcut icon" href="{{url('images/favicon.png')}}">
+        <link href="{{url('css/mdb.css')}}" rel="stylesheet">
         <link href="{{url('css/modern-business.css')}}" rel="stylesheet">
+        <link href="{{url('css/customstyles.css')}}" rel="stylesheet">
         <!-- Styles -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
         <style>
             
         </style>
     </head>
     <body>
         <div id="app"></div>
+
+       
     </body>
 
     <script src="{{url('js/app.js')}}"></script>
+    <script src="{{url('js/agency.min.js')}}"></script>
 </html>
