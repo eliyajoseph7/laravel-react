9ca04cb97b74f45397ea57f3f34f176afdca5e0e
eliyajoseph7 <exaveryeliya20@gmail.com>
2020-04-21 20:56:11 +0300

    little modifications added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,22 +5,33 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 
-        <title>MORUWASA</title>
+        <title>GWUS</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         <!-- Fontawesome 5 -->
-        <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-
+        <!-- <script src="{{url('js/kit.fontawesome.js')}}"></script> -->
+        <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+      crossorigin="anonymous"
+    />
+    <style>
+        img{
+            object-fit: cover;
+        }
+    </style>
 
         <link href="{{url('css/app.css')}}" rel="stylesheet">
         <!-- Favicon-->
         <link rel="shortcut icon" href="{{url('images/favicon.png')}}">
-        <link href="{{url('css/mdb.css')}}" rel="stylesheet">
+        <!-- <link href="{{url('css/mdb.css')}}" rel="stylesheet"> -->
         <link href="{{url('css/modern-business.css')}}" rel="stylesheet">
         <link href="{{url('css/customstyles.css')}}" rel="stylesheet">
+        <link href="{{url('css/styles.css')}}" rel="stylesheet">
         <!-- Styles -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+        <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"> -->
 
         <style>
             
@@ -34,4 +45,8 @@
 
     <script src="{{url('js/app.js')}}"></script>
     <script src="{{url('js/agency.min.js')}}"></script>
+    <script src="{{url('js/scriptsAdmin.js')}}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js" crossorigin="anonymous"></script>
+    <!-- <script src="https://kit.fontawesome.com/a076d05399.js"></script> -->
+
 </html>
