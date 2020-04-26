6f14ede691cc5d4fba72168f9380bf5b154f6818
eliyajoseph7 <exaveryeliya20@gmail.com>
2020-04-26 21:28:14 +0300

    Routes added and modified

--- resources/js/componentsadmin/Index.js
+++ resources/js/componentsadmin/Index.js
@@ -4,15 +4,32 @@ import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Header from './layouts/Header';
 import Menu from './layouts/Menu';
 import Footer from './layouts/Footer';
+import Dashboard from './Dashboard';
+import UpdateProduct from './dashboardComponents/UpdateProduct';
+import AddProduct from './dashboardComponents/AddProduct';
 
 
 export default class Index extends Component {
     render() {
         return (
             <div>
-                <Header />
-                <Menu />
-                <Footer />
+                <Router>
+                    <Header />
+                    <Menu />
+                    
+                    <Route exact path='/admin'><Dashboard /></Route> 
+                    <Route exact path='/admin/edit-product/:id'>
+                        <UpdateProduct />
+                    </Route> 
+                    <Route exact path="/admin/add-product">
+                        <AddProduct />
+                    </Route> 
+                    <Route exact path="/products/:id">
+                        <Dashboard />
+                    </Route> 
+                    <Footer />
+                </Router>
+                
             </div>
         );
     }
