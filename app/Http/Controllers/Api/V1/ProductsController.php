9bba6219903afe345b5e8a6afa909e41137c3889
eliyajoseph7 <exaveryeliya20@gmail.com>
2020-05-01 20:19:59 +0300

    api update fuction cteated and show function updated

--- app/Http/Controllers/Api/V1/ProductsController.php
+++ app/Http/Controllers/Api/V1/ProductsController.php
@@ -6,7 +6,9 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ResourcesCollections\ProductsResourceCollection;
 use App\Http\Resources\ProductsResource;
+use Illuminate\Support\Facades\Input;
 use App\Product;
+use App\ProductCategory;
 
 class ProductsController extends Controller
 {
@@ -19,7 +21,26 @@ class ProductsController extends Controller
 
     public function show($id): ProductsResource
     {
-        $products = Product::find($id);
+        $products = Product::join('product_categories', 'products.category_id', '=', 'product_categories.id')
+                            ->where('products.id', $id)
+                            ->first();
+        
+        // return $products;
         return new ProductsResource($products);
+    }
+
+    public function update(Product $product, Request $request) : ProductsResource
+    {
+
+       
+
+        $product -> update($request -> all());
+        $newCategory = $request -> input('category');
+        $category = new ProductCategory;
+        $category = $category::where('id', $product->category->id)->first();
+        $category->category = $newCategory;
+        $category->save();
+        return new ProductsResource($product);
+        
     }
 }
