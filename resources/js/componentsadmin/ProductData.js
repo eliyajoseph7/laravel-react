24fc5487726fc9c79beae401db15bf20f36c61a6
eliyajoseph7 <exaveryeliya20@gmail.com>
2020-05-07 21:11:46 +0300

    codes modified

--- resources/js/componentsadmin/ProductData.js
+++ resources/js/componentsadmin/ProductData.js
@@ -3,28 +3,26 @@ import { Link } from 'react-router-dom'
 import useAxiosGet from '../components/Hooks/HttpRequests'
 
 let count = 1
-function ProductData(props) {
+function ProductData(product) {
     return (
         <React.Fragment>
             {
                 
-                props.products.map((product) =>
                 
-                <tr key={product.id}>
+                <tr>
                    
                     <td>{ count++ }</td>
-                    <td style={{width: "150px"}}><img src={product.image} className="img-thumbnail" /></td>
-                    <td>{product.product_name}</td>
-                    <td>{product.description}</td>
-                    <td>{product.price}</td>
+                    <td style={{width: "150px"}}><img src={product.product.image} className="img-thumbnail" /></td>
+                    <td>{product.product.product_name}</td>
+                    <td>{product.product.description}</td>
+                    <td>{product.product.price}</td>
                     <td className="justify-content-between" style={{width: "100px"}}>
-                        <span className="badge bg-danger"><Link to={`/admin/edit-product/${product.id}`}>update</Link></span>
+                        <span className="badge bg-danger"><Link to={`/admin/edit-product/${product.product.id}`}>update</Link></span>
                         {/* <span className="badge m-auto"><i className="fa fa-trash"></i></span> */}
                     </td>
                     
                 </tr>
-                
-                )
+             
             }
              
         </React.Fragment>
