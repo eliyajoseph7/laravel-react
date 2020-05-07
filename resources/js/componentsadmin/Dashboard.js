fedea1c5f7d9fe6ea82861f31b71559037b947e2
eliyajoseph7 <exaveryeliya20@gmail.com>
2020-05-07 21:09:35 +0300

    chart for the product sales added

--- resources/js/componentsadmin/Dashboard.js
+++ resources/js/componentsadmin/Dashboard.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react'
 import axios from 'axios';
 import Products from './dashboardComponents/Products';
+import ProductSalesChart from './charts/ProductSalesChart';
 
 export default class Dashboard extends Component {
 
-    // constructor(){
-    //     super()
-    //     this.state = {
-    //         news: []
-    //     }
-    // }
+    constructor(){
+        super()
+        this.state = {
+            salesData: {}
+        }
+    }
 
     // componentDidMount() {
     //     const script = document.createElement('script');
@@ -27,12 +28,37 @@ export default class Dashboard extends Component {
     //         });
         
     // }
+    componentWillMount(){
+        this.getSalesData();
+    }
 
+    getSalesData(){
+        // Ajax request here
+        this.setState({
+            salesData: {
+                labels: ['software', 'hardware', 'autoparts', 'electronics', 'agriculture'],
+                datasets: [
+                    {
+                        label: 'Product Sales',
+                        data: [
+                            '72847',
+                            '7883',
+                            '72847',
+                            '7532',
+                            '72647',
+                        ],
+                        backgroundColor: ['green', 'yellow', 'gold', 'violet', 'pink']
+                    },
+                ]
+            }
+        })
+    }
     
 
     render() {
         return (
             <div className="content-wrapper">
+                <ProductSalesChart salesData={this.state.salesData} legendPosition="bottom"/>
                 <Products />
             </div>
 
